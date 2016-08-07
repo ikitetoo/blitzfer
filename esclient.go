7eb0b2e7d041d86b83c2ac6c0ad0c246687091cc
Dennis T. Morsani Jr <dennis@morsani.net>
2016-08-06 23:56:33 -0400

    Spent more time on ElasticSearch payload.

--- esclient.go
+++ esclient.go
@@ -1,6 +1,7 @@
 package main
 import ( "fmt"
 	 "os"
+	 "os/user"
 	 "log"
 	 "time"
 	 "syscall"
@@ -9,6 +10,7 @@ import ( "fmt"
 
 func escConnect() {
 	// Create a client and connect to http://192.168.2.10:9201
+	// TODO: This needs to go into a config file... preferably .yml
         log.Printf("Connecting to http://192.168.99.100:9200 ...\n")
         esc, err := elastic.NewClient(
                     elastic.SetURL("http://192.168.99.100:9200"),
@@ -47,33 +49,67 @@ func escConnect() {
 
 func escUpdate(node FsMetaData) {
 
-        // Shove this into Elasticsearch
+        // Determine node ownerships.
+	nodeUid := node.info.Sys().(*syscall.Stat_t).Uid
+	nodeGid := node.info.Sys().(*syscall.Stat_t).Gid
+
+        // Need to convert these for LookupId, derp.
+        nodeUidString := fmt.Sprint(nodeUid)
+        nodeGidString := fmt.Sprint(nodeGid)
+
+        // Test to see if the Owner UID already exists in memory/map. Otherwise add it.
+	_, nodeUidExists := uidToNameMap[nodeUid]
+	if (nodeUidExists) {
+		fmt.Printf("[%v] exists in map\n", nodeUid)
+	} else {
+		// Insert uid[username] into map.
+		u, _ := user.LookupId(nodeUidString)
+		uidToNameMap[nodeUid] = u.Username
+		fmt.Printf("will insert [%v / %v] into our user map\n", nodeUid, u.Username)
+	}
+
+        // Rinse, repeat for GID table.
+	_, nodeGidExists := gidToNameMap[nodeGid]
+	if (nodeGidExists) {
+		fmt.Printf("[%v] exists in map\n", nodeGid)
+	} else {
+		// Insert gid[gidname] into map.
+		g, _ := user.LookupId(nodeGidString)
+		gidToNameMap[nodeGid] = g.Username
+		fmt.Printf("will insert [%v / %v] into our gid map\n", nodeGid, g.Username)
+	}
+
+        // Shove this into Elasticsearch.
 	type esDoc struct {
-		modTime         time.Time
-		isDir           bool
-		parentDirectory	string
-		absPath         string
-//              ownerName       string
-//              gidName         string
-                ownerId         uint32
-                gidId           uint32
-                perms           os.FileMode
+		ModTime         time.Time
+		IsDir           bool
+		ParentDirectory	string
+		AbsPath         string
+                OwnerId         uint32
+                GidId           uint32
+                Perms           os.FileMode
+                OwnerName       string
+                GidName         string
 	}
 
+        // Create out Elasticsearch document payload.
         doc := esDoc {
-		modTime:         node.info.ModTime(),
-		isDir:           node.info.IsDir(),
-		parentDirectory: node.parent,
-                absPath:         node.path,
-		ownerId:         node.info.Sys().(*syscall.Stat_t).Uid,
-		gidId:           node.info.Sys().(*syscall.Stat_t).Gid,
-		perms:           node.mode,
+		ModTime:         node.info.ModTime(),
+		IsDir:           node.info.IsDir(),
+		ParentDirectory: node.parent,
+                AbsPath:         node.path,
+		OwnerId:         nodeUid,
+		GidId:           nodeGid,
+		Perms:           node.mode,
+		OwnerName:       uidToNameMap[nodeUid],
+		GidName:         gidToNameMap[nodeGid],
 	}
 
+	// Marshal the data prior to sending it up to Elastic Search.
         b, err := json.Marshal(doc)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
 
-	fmt.Printf("-----> %v", b)
+	fmt.Printf("Elastic Search Payload: %s\n", b)
 }
