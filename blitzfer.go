3ad7c5a3fd33835573cdae7c4cc67fd7a94af676
Dennis T. Morsani Jr <dennis@morsani.net>
2016-08-06 22:01:28 -0400

    Adding uid/gid to name maps.

--- blitzfer.go
+++ blitzfer.go
@@ -23,12 +23,21 @@ var aq chan FsMetaData = make(chan FsMetaData)
 // Allow us to wait on routines to finish.
 var done sync.WaitGroup
 
+// Create a uid Dictionary, to limit lookups.
+var uidToNameMap map[uint32]string
+var gidToNameMap map[uint32]string
+
 const (
         version = "0.0.1"
 )
 
 func main() {
 	var source_path string
+
+        // Allocate map structures.
+        uidToNameMap = make(map[uint32]string)
+        gidToNameMap = make(map[uint32]string)
+
 
 	flag.BoolVar(&debug, "verbose", false, "Verbose output")
 	flag.StringVar(&source_path, "directory", ".", "Path of directory to scan.")
--- esclient.go
+++ esclient.go
@@ -1,7 +1,6 @@
 package main
 import ( "fmt"
 	 "os"
-	 "os/user"
 	 "log"
 	 "time"
 	 "syscall"
