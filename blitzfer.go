0a6cb71db73a6cb67540b35e9496ea9cd42c8d83
Dennis T. Morsani Jr <dennis@morsani.net>
2016-08-08 20:38:21 -0400

    Adding yaml config support!

--- blitzfer.go
+++ blitzfer.go
@@ -9,10 +9,13 @@ import (
         "github.com/olivere/elastic"
 )
 
+// Main Variables
 var debug bool
 var ver   bool
 var max   int
 var esc   *elastic.Client
+var sourcePath string
+var configFile string
 
 // Passive Queue channel
 var pq chan FsMetaData = make(chan FsMetaData)
@@ -31,16 +34,17 @@ const (
         version = "0.1.0"
 )
 
-func main() {
-	var source_path string
+var myConfigs *Config
 
+func main() {
         // Allocate map structures.
         uidToNameMap = make(map[uint32]string)
         gidToNameMap = make(map[uint32]string)
 
-
+        // Add default config path.
+        flag.StringVar(&configFile, "configFile", "./config.yml", "Path of config file.")
 	flag.BoolVar(&debug, "verbose", false, "Verbose output")
-	flag.StringVar(&source_path, "directory", ".", "Path of directory to scan.")
+	flag.StringVar(&sourcePath, "directory", ".", "Path of directory to scan.")
 	flag.IntVar(&max, "max", 100, "Max number of concurently open directories.")
 	flag.BoolVar(&ver, "version", false, "Version output")
 	flag.Parse()
@@ -53,11 +57,16 @@ func main() {
 	// listen for new directories.
         go passiveQueue()
 
+	// load configuration map.
+	myConfigs = loadConfig()
+
+	os.Exit(0)
+
 	// test elasticsearch connection.
 	esc = escConnect()
 
 	// Start directory scanning.
-	scanInit(source_path)
+	scanInit(sourcePath)
 
 	log.Printf("Done.\n")
 	os.Exit(0)
--- config.go
+++ config.go
@@ -2,9 +2,47 @@ package main
 
 import (
         "fmt"
-        "log"
-
+	"path/filepath"
+	"io/ioutil"
         "gopkg.in/yaml.v2"
 )
 
-func readConfig() *
+// Defaults and Elasticsearch configs
+type Config struct {
+    Configs map[string]Options
+    Elasticsearch map[string]Elasticsearch
+}
+
+type Options struct {
+    Debug     bool
+    Directory string
+}
+
+type Elasticsearch struct {
+    Index     string
+    Ip        string
+    Port      int
+}
+
+func loadConfig() *Config {
+
+    // Read our config file
+    filename, _ := filepath.Abs(configFile)
+    yamlFile, err := ioutil.ReadFile(filename)
+
+    if err != nil {
+        panic(err)
+    }
+
+    var config Config
+
+    // UnMarshal the config into our config structure.
+    err = yaml.Unmarshal(yamlFile, &config)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Value: %#v\n", config.Configs)
+
+    return &config
+}
